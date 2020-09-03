25ea6c55f7f6756532e7f92b7e4ad890760a4aa0
HexTheCode <alopezgarcia380@gmail.com>
2020-09-03 13:39:14 +0200

    Api request and more :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 function App() {
+  // State
+
+  const [busqueda, saveBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const [consultar, saveConsultar] = useState(false);
+
+  const [resultado, saveResultado] = useState({});
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const requestApi = async () => {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=a68df70ae1b03d0b98b8b63072628120`;
+
+      const res = await fetch(url);
+      const json = await res.json();
+
+      saveResultado(json);
+      saveConsultar(false);
+    };
+    requestApi();
+  }, [consultar]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header title="Weather App" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Form
+                busqueda={busqueda}
+                saveBusqueda={saveBusqueda}
+                saveConsultar={saveConsultar}
+              />
+            </div>
+            <div className="col m6 s12">
+              <Weather resultado={resultado} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+const Form = ({ busqueda, saveBusqueda, saveConsultar }) => {
+  const [error, saveError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  const handleChange = (e) => {
+    saveBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (ciudad.trim() === "" || pais.trim() === "") {
+      saveError(true);
+      return;
+    }
+    saveError(false);
+
+    saveConsultar(true);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error ? (
+        <p className="red darken-4 error">Todos los campos son obligatorios</p>
+      ) : null}
+      <div className="input-field col s12">
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
+        <label htmlFor="ciudad">City</label>
+      </div>
+      <div className="input-field col s12">
+        <select name="pais" id="pais" value={pais} onChange={handleChange}>
+          <option value="">-- Select Country --</option>
+          <option value="US">USA</option>
+          <option value="MX">Mexico</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">Spain</option>
+          <option value="PE">Perú</option>
+        </select>
+        <label htmlFor="pais">Country</label>
+      </div>
+      <div className="input-field col s12">
+        <button
+          className="waves-effect waves-light btn-large btn-block yellow accent-4 col s12"
+          type="submit"
+          style={{ color: "black" }}
+        >
+          Check
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
